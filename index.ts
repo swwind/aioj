2939eca56549d345a64958eaa88a3b1cd57a1e3a
swwind <swwind233@gmail.com>
2020-11-26 12:04:22 +0800

    backend: add hsts

--- index.ts
+++ index.ts
@@ -2,14 +2,40 @@ import Koa from 'koa';
 import router from './app/router';
 import body from 'koa-body';
 import serve from 'koa-static';
+import config from './config.json';
+import https from 'https';
+import { promises as fs } from 'fs';
 
 const app = new Koa();
 
 app.use(body({ multipart: true }));
 app.use(serve('dist'));
+app.use(serve('static'));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(8080);
-console.log('listening on http://localhost:8080');
+if (config.port === 443) {
+  const server = https.createServer({
+    key: await fs.readFile(config.https.key),
+    cert: await fs.readFile(config.https.cert),
+  }, app.callback());
+  server.listen(config.port);
+
+  console.log('Congradulations! Server started on https://localhost');
+
+  if (config.https.hsts) {
+    const jump = new Koa();
+    jump.use(async (ctx) => {
+      ctx.redirect('https://' + ctx.host + ctx.url);
+    });
+    jump.listen(80);
+
+    console.log('And HSTS server started on http://localhost');
+  }
+} else {
+  app.listen(config.port);
+
+  console.log(`Server started on http://localhost:${config.port}`);
+}
+
